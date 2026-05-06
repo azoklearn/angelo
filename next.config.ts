9f19c927a4fea00441ed03e65c86c296bef78010
azoklearn <152606904+azoklearn@users.noreply.github.com>
2026-05-06 17:50:17 +0200

    Add files via upload

---
+++ next.config.ts
@@ -0,0 +1,11 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  outputFileTracingRoot: process.cwd(),
+  images: {
+    formats: ["image/avif", "image/webp"],
+    unoptimized: true,
+  },
+};
+
+export default nextConfig;
